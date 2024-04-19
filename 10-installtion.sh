1a04cd42c9f395edc2904f2dffed82b0baf3d9b7
raj <raj.pradeep676@gmail.com>
2024-04-19 17:24:56 +0530

    install

--- 10-installtion.sh
+++ 10-installtion.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+UESRID=$(id -u)
 dnf install mysql -y
+if [USERID -ne 0]
+then 
+echo "please run this script with root access"
+else
+
+echo "your are super user"
+fi
+dnf install mysql -y 
