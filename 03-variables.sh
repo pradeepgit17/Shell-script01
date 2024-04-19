d8b1fb4834d80edcb4fcf722a358a5663b0cf747
raj <raj.pradeep676@gmail.com>
2024-04-19 15:47:38 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,4 +6,4 @@ person2=pradeep
 echo "$person1:: Hello $person2, How are you?"
 echo "$person2:: Hi $person1, I am fine. How are you?"
 echo "$person1: I am fine too. how is your work?"
-echo "S$person2:: not bad. I am thinking to upgrade to DevOps"
+echo "$person2:: not bad. I am thinking to upgrade to DevOps"
