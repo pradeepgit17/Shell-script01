32085d1974ae756322195da25b68b3ea92c1893b
raj <raj.pradeep676@gmail.com>
2024-04-19 16:53:16 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,8 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
 then
-echo "Given nuber $NUMBER IS GREATER THAN 10"
+echo "Given number $NUMBER IS GREATER THAN 10"
 else
-echo "Given nuber $NUMBER IS LESS THAN 10"
+echo "Given number $NUMBER IS LESS THAN 10"
 
 fi
