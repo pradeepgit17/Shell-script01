56889d4fdfd52eb988372ec7f11bdef0f8400175
raj <raj.pradeep676@gmail.com>
2024-04-19 15:37:43 +0530

    hello world

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "pradeep:: Hello Sanjay, How are you?"
+echo "Sanjay:: Hi pradeep, I am fine. How are you?"
+echo "pradeep: I am fine too. how is your work?"
+echo "Sanjay:: not bad. I am thinking to upgrade to DevOps"
